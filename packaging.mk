d3610034a1032a249446bb9e56e595df864b633f
Michael Rigoni <michael.rigoni@gmail.com>
2013-08-09 16:58:11 +0200

    fix missing dependencies

--- packaging.mk
+++ packaging.mk
@@ -39,7 +39,7 @@ endif
 
 # Make sure to regenerate changelog each time to add EPOCH time of build
 .PHONY: debian/changelog
-debian/changelog: debian/changelog.in
+debian/changelog: debian/changelog.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 	@if [ "$(ORIGIN_TARGET)" = "stable" ]; \
 	then \
@@ -49,19 +49,19 @@ debian/changelog: debian/changelog.in
 		perl -p -i -e '$$now=time(); s:\((.*)\):($${1}~$$now): if 1 .. 1' $@ ;\
 	fi
 
-debian/control: debian/control.in
+debian/control: debian/control.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 
-debian/preinst: debian/preinst.in
+debian/preinst: debian/preinst.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 
-debian/postinst: debian/postinst.in
+debian/postinst: debian/postinst.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 
-debian/prerm: debian/prerm.in
+debian/prerm: debian/prerm.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 
-debian/postrm: debian/postrm.in
+debian/postrm: debian/postrm.in Makefile
 	$(call gen-from-tmpl,$<,$@)
 
 PACKAGE-VERSION = $(shell head -1 debian/changelog | cut -f 2 -d \( | cut -f 1 -d \))
