6b9a2a6f0e99a0bce616ba39d2344b42761f2dea
Michael Rigoni <michael.rigoni@origin-nexus.com>
2014-06-04 12:15:12 +0200

    simplify code and use more reliable interprocess communication

--- origin/nac.py
+++ origin/nac.py
@@ -2,25 +2,20 @@ from origin.synapse import Synapse
 import json, subprocess
 
 ALLOWED_MACS_SETS_PATH = 'allowed_macs:'
-AUTHORIZATION_CHANNEL_PATH = 'vlan_mac_authorizations:'
-ALLOW_AUTHORIZATION_CHANNEL = AUTHORIZATION_CHANNEL_PATH + 'allow'
-DISALLOW_AUTHORIZATION_CHANNEL = AUTHORIZATION_CHANNEL_PATH + 'disallow'
+AUTHORIZATION_CHANNEL = 'vlan_mac_authorizations'
 
 def macAllowed(mac, vlan):
     return Synapse().sismember(ALLOWED_MACS_SETS_PATH + str(vlan), mac)
 
 def allowMAC(mac, vlan):
-    Synapse().publish(ALLOW_AUTHORIZATION_CHANNEL, { 'macs' : [mac], 'vlans': [vlan] } )
+    Synapse().lpush(AUTHORIZATION_CHANNEL, { 'mac' : mac, 'vlan': vlan, 'action': 'allow' } )
     
 def disallowMAC(mac, vlan):
-    Synapse().publish(DISALLOW_AUTHORIZATION_CHANNEL, { 'macs' : [mac], 'vlans': [vlan] } )
+    Synapse().lpush(AUTHORIZATION_CHANNEL, { 'mac' : mac, 'vlan': vlan, 'action': 'disallow' } )
 
 class FirewallConfigurator:
     def __init__(self):
         self.synapse = Synapse()
-        self.pubsub = self.synapse.pubsub() 
-
-        self.pubsub.psubscribe([AUTHORIZATION_CHANNEL_PATH + '*'])
         self.init()
         
     def init(self):
@@ -35,21 +30,16 @@ class FirewallConfigurator:
         subprocess.call(cmd, shell=True)
 
     def run(self):
-        for item in self.pubsub.listen():
-            if item['channel'] == ALLOW_AUTHORIZATION_CHANNEL or item['channel'] == DISALLOW_AUTHORIZATION_CHANNEL:
-                data = json.loads(item['data']) # TODO: JSON decoding should be wrapped in Synapse some way...
-                if item['channel'] == DISALLOW_AUTHORIZATION_CHANNEL:
-                    command = 'delete'
-                else:
-                    command = 'add'
-                cmd = ''
-                for mac in data['macs']:
-                    for vlan in data['vlans']:
-                        for family in ['bridge', 'ip', 'ip6']:
-                            cmd += 'nft {command} element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=vlan, mac=mac, command=command) 
-                for vlan in data['vlans']:
-                    if command == 'add':
-                        self.synapse.sadd(ALLOWED_MACS_SETS_PATH + str(vlan), *data['macs'])
-                    else:
-                        self.synapse.srem(ALLOWED_MACS_SETS_PATH + str(vlan), *data['macs'])
-                subprocess.call(cmd, shell=True)
+        while True:
+            item = self.synapse.brpop(AUTHORIZATION_CHANNEL)[1]
+            if item['action'] == 'allow':
+                command = 'add'
+                self.synapse.sadd(ALLOWED_MACS_SETS_PATH + str(item['vlan']), item['mac'])
+            else:
+                command = 'delete'
+                self.synapse.srem(ALLOWED_MACS_SETS_PATH + str(item['vlan']), item['mac'])
+            
+            cmd = ''
+            for family in ['bridge', 'ip', 'ip6']:
+                cmd += 'nft {command} element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=item['vlan'], mac=item['mac'], command=command) 
+            subprocess.call(cmd, shell=True)
