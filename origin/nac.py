3971285771d7303ccd14c91111a3e9190116e773
Michael Rigoni <michael.rigoni@origin-nexus.com>
2014-06-04 10:47:04 +0200

    on startup, allow macs that were allowed

--- origin/nac.py
+++ origin/nac.py
@@ -21,11 +21,23 @@ class FirewallConfigurator:
         self.pubsub = self.synapse.pubsub() 
 
         self.pubsub.psubscribe([AUTHORIZATION_CHANNEL_PATH + '*'])
+        self.init()
+        
+    def init(self):
+        # on startup, initialize sets
+        # TODO: this should be done in a transaction but 'nft flush set ...' does not work... 
+        cmd = ''
+        for key in self.synapse.keys( ALLOWED_MACS_SETS_PATH + '*'):
+            for mac in self.synapse.smembers(key):
+                vlan = key.replace(ALLOWED_MACS_SETS_PATH, '')
+                for family in ['bridge', 'ip', 'ip6']:
+                    cmd += 'nft add element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=vlan, mac=mac) 
+        subprocess.call(cmd, shell=True)
 
     def run(self):
         for item in self.pubsub.listen():
             if item['channel'] == ALLOW_AUTHORIZATION_CHANNEL or item['channel'] == DISALLOW_AUTHORIZATION_CHANNEL:
-                data = json.loads(item['data']) # TODO: this should be wrapped in Synapse some way...
+                data = json.loads(item['data']) # TODO: JSON decoding should be wrapped in Synapse some way...
                 if item['channel'] == DISALLOW_AUTHORIZATION_CHANNEL:
                     command = 'delete'
                 else:
@@ -35,9 +47,9 @@ class FirewallConfigurator:
                     for vlan in data['vlans']:
                         for family in ['bridge', 'ip', 'ip6']:
                             cmd += 'nft {command} element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=vlan, mac=mac, command=command) 
-                subprocess.call(cmd, shell=True)
                 for vlan in data['vlans']:
                     if command == 'add':
                         self.synapse.sadd(ALLOWED_MACS_SETS_PATH + str(vlan), *data['macs'])
                     else:
                         self.synapse.srem(ALLOWED_MACS_SETS_PATH + str(vlan), *data['macs'])
+                subprocess.call(cmd, shell=True)
