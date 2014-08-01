ef1cf13d7c4616b66a6b992fcace8b0a50300d8b
Michael Rigoni <michael.rigoni@origin-nexus.com>
2014-08-01 17:44:16 +0200

    use CC authorization sessions

--- origin/nac.py
+++ origin/nac.py
@@ -1,5 +1,9 @@
 from origin.neuron import Synapse, Dendrite
+from origin import session
 import subprocess, datetime, re
+
+DISCONNECT_NOTIFICATION_PATH = 'device:vlan_mac_disconnected' # TODO Factorize: also in session_trackerd
+SESSION_END_NOTIFICATION_PATH = 'device:session_end_notification' # same
 
 ALLOWED_MACS_PATH_PATTERN = 'vlan:{vlan}:allowed_macs' # sorted set with epoch expiry time (or inf) as score. ALL authorized macs are in there, even thos that expire on disconnect
 DISALLOWED_MACS_ON_DISCONNECT_PATH_PATTERN = 'vlan:{vlan}:disallowed_macs_on_disconnect' # set
@@ -31,17 +35,18 @@ def allowMAC(mac, vlan, till_date=None, disallow_mac_on_disconnect=False):
         params['till_date'] = tzaware_datetime_to_epoch(till_date) # Pass as EPOCH
     Synapse().lpush(AUTHORIZATION_CHANNEL, params)
     
-def disallowMAC(mac, vlan):
-    Synapse().lpush(AUTHORIZATION_CHANNEL, { 'mac' : mac, 'vlan': vlan, 'action': 'disallow' } )
+def disallowMAC(mac, vlan, reason):
+    Synapse().lpush(AUTHORIZATION_CHANNEL, { 'mac' : mac, 'vlan': vlan, 'action': 'disallow', 'reason': reason } )
 
 class MacAuthorizationManager(Dendrite):
     def __init__(self):
         super().__init__(self, 'firewall')
         
         self.add_channel(AUTHORIZATION_CHANNEL, self.handle_request)
+        self.add_channel(DISCONNECT_NOTIFICATION_PATH, self.handle_disconnection)
 
         # get VLANS that are managed by controller.
-        self.vlans = [re.search(ALLOWED_MACS_PATH_PATTERN.format(vlan='(?P<vlan>.+)'), key).group('vlan') for key in self.synapse.keys( ALLOWED_MACS_PATH_PATTERN.format(vlan='*') )]
+        self.vlans = set(re.search(ALLOWED_MACS_PATH_PATTERN.format(vlan='(?P<vlan>.+)'), key).group('vlan') for key in self.synapse.keys( ALLOWED_MACS_PATH_PATTERN.format(vlan='*') ))
 
         self.check_expired_authz()
 
@@ -59,6 +64,8 @@ class MacAuthorizationManager(Dendrite):
         subprocess.call(cmd, shell=True)
 
     def allow_mac(self, mac, vlan, till_date, disallow_mac_on_disconnect):
+        ''' Allows mac on VLAN for period and/or till disconnection. *NO* notification is sent to CC: it is the responsibility of the authorizer'''
+        self.vlans.add(vlan)
         self.synapse.zadd(ALLOWED_MACS_PATH_PATTERN.format(vlan=vlan), till_date, mac)
         if disallow_mac_on_disconnect:
             self.synapse.sadd(DISALLOWED_MACS_ON_DISCONNECT_PATH_PATTERN.format(vlan=vlan), mac)
@@ -70,8 +77,10 @@ class MacAuthorizationManager(Dendrite):
         for family in ['bridge', 'ip', 'ip6']:
             cmd += 'nft add element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=vlan, mac=mac) 
         subprocess.call(cmd, shell=True)
+        
 
-    def disallow_mac(self, mac, vlan):
+    def disallow_mac(self, mac, vlan, reason='expired'):
+        ''' Disallows MAC on VLAN and ends session on CC  (unless reason is revoked, because revocations are made on CC)'''
         self.synapse.zrem(ALLOWED_MACS_PATH_PATTERN.format(vlan=vlan), mac)
         self.synapse.srem(DISALLOWED_MACS_ON_DISCONNECT_PATH_PATTERN.format(vlan=vlan), mac)
 
@@ -80,6 +89,10 @@ class MacAuthorizationManager(Dendrite):
             cmd += 'nft delete element {family} origin a_v_{vlan} {{{mac}}};'.format(family=family, vlan=vlan, mac=mac) 
         subprocess.call(cmd, shell=True)
         #TODO: Flush connections with conntrack (get IPs of MAC and conntrack -D -s <IP>)
+        
+        # end session
+        if reason != 'revoked':
+            session.end_authorization_session(mac, vlan, termination_reason=reason)
             
     def handle_request(self, item):
         if item['action'] == 'allow':
@@ -87,10 +100,18 @@ class MacAuthorizationManager(Dendrite):
             self.allow_mac(mac=item['mac'], vlan=item['vlan'], till_date=till_date, disallow_mac_on_disconnect=item['disallow_mac_on_disconnect'])
 
         else:
-            self.disallow_mac(mac=item['mac'], vlan=item['vlan'])
+            self.disallow_mac(mac=item['mac'], vlan=item['vlan'], reason=item['reason'])
         
         # Check if authz have expired and set correct timeout 
         self.check_expired_authz()
+
+    def handle_disconnection(self, item):
+        if self.synapse.sismember( DISALLOWED_MACS_ON_DISCONNECT_PATH_PATTERN.format(vlan=item['vlan']), item['mac'] ):
+            self.disallow_mac(mac=item['mac'], vlan=item['vlan'], reason='disconnected')
+        
+        # Check if authz have expired and set correct timeout 
+        self.check_expired_authz()
+
     
     def timeout_cb(self):
         self.check_expired_authz()
@@ -101,7 +122,7 @@ class MacAuthorizationManager(Dendrite):
         for vlan in self.vlans:
             key = ALLOWED_MACS_PATH_PATTERN.format(vlan=vlan)
             for mac in self.synapse.zrangebyscore(key, float('-inf'), now):
-                self.disallow_mac(mac, vlan)
+                self.disallow_mac(mac, vlan, reason='expired')
             
             # get next mac to expire
             for mac, epoch_expire in self.synapse.zrange(key, 0, 0, withscores=True): # returns first mac to expire
