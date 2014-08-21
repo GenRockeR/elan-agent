3d4998bfd24222ee972b0641fd1d6403e0a7d9cb
Michael Rigoni <michael.rigoni@origin-nexus.com>
2014-08-21 12:57:06 +0200

    grab VLAN directly in  device tracker. Use libpcap instead of nflog as nflog strips out vlan information....

--- device_trackerd.py
+++ device_trackerd.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 from origin.utils import if_indextoname
-import os
 
-DEVICE_NFLOG_QUEUE = int(os.environ.get('DEVICE_NFLOG_QUEUE', 10))
 LAST_SEEN_PATH = 'device:last_seen' # TODO factorize somewhere
 
 
@@ -16,13 +14,21 @@ def getPacketParams(packet):
     while packet:
         if packet.__class__.__name__ == 'Ethernet':
             params['mac'] = packet.as_eth_addr(packet.get_ether_shost())
+            try:
+                # Todo: Treat Q in Q ....
+                params['vlan'] = packet.get_tag(0).get_vid()
+            except IndexError:
+                params['vlan'] = 0
+                
         elif packet.__class__.__name__ == 'ARP':
             if packet.get_ar_pro() == 0x800 and packet.get_ar_op() in (1,2): # IP protocol and ARP request or reply
                 params['ip'] = packet.as_pro(packet.get_ar_spa())
                 break
+            
         elif packet.__class__.__name__ == 'IP':
             params['ip'] = packet.get_ip_src()
             break
+        
         elif packet.__class__.__name__ == 'IP6':
             params['ip'] = packet.get_source_address().as_string()
             break
@@ -59,56 +65,57 @@ def ignoreIP(ip):
     
     return False
 
+def process_packet(pktlen, data, timestamp):
+    if not data:
+        return
+
+    try:
+        pkt_obj = decoder.decode(data)
+        pkt_params = getPacketParams(pkt_obj)
+        if ignoreMAC(pkt_params['mac']):
+            return
+        
+        now = (datetime.datetime.fromtimestamp(timestamp) - datetime.datetime(1970, 1, 1)).total_seconds() # EPOCH
+
+        args = [ now, dict(mac=pkt_params['mac']), 
+                 now, dict(mac=pkt_params['mac'],vlan=pkt_params['vlan'])
+               ]
+        
+        use_ip = False
+        if 'ip' in pkt_params and not ignoreIP(pkt_params['ip']):
+                use_ip = True
+                args.extend( (now, pkt_params) )
+        
+        nb_added = synapse.zadd(LAST_SEEN_PATH, *args)
+        
+        if nb_added: # new session(s) created: inform Control Center
+            if use_ip:
+                session.start_IP_session(mac=pkt_params['mac'], vlan=pkt_params['vlan'], ip=pkt_params['ip'], start=now)
+            else:
+                session.start_VLAN_session(mac=pkt_params['mac'], vlan=pkt_params['vlan'], start=now)
+
+    except:
+        traceback.print_exc()
+
 
 if __name__ == '__main__':
-    import origin.libnflog_cffi
     import signal
     import datetime
     import traceback
     from origin.neuron import Synapse
     from origin import session
     from impacket.ImpactDecoder import EthDecoder
+    import pcap
     
     synapse = Synapse()
-
-    nflog = origin.libnflog_cffi.NFLOG().generator(DEVICE_NFLOG_QUEUE, extra_attrs=['msg_packet_hwhdr', 'physindev'], nlbufsiz=2**24, handle_overflows = False)
-    fd = next(nflog)
-    
+ 
     decoder = EthDecoder()
     
-    for pkt, hwhdr, physindev  in nflog:
-        try:
-            pkt_obj = decoder.decode(hwhdr + pkt)
-            pkt_params = getPacketParams(pkt_obj)
-            if ignoreMAC(pkt_params['mac']):
-                continue
-            pkt_params['vlan'] = 0
-            iface = if_indextoname(physindev)
-            if '.' in iface:
-                pkt_params['vlan'] = iface.split('.')[1]
-            
-            now = (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() # EPOCH
-
-            args = [ now, dict(mac=pkt_params['mac']), 
-                     now, dict(mac=pkt_params['mac'],vlan=pkt_params['vlan'])
-                   ]
-            
-            use_ip = False
-            if 'ip' in pkt_params and not ignoreIP(pkt_params['ip']):
-                    use_ip = True
-                    args.extend( (now, pkt_params) )
-            
-            nb_added = synapse.zadd(LAST_SEEN_PATH, *args)
-            
-            if nb_added: # new session(s) created: inform Control Center
-                if use_ip:
-                    session.start_IP_session(mac=pkt_params['mac'], vlan=pkt_params['vlan'], ip=pkt_params['ip'], start=now)
-                else:
-                    session.start_VLAN_session(mac=pkt_params['mac'], vlan=pkt_params['vlan'], start=now)
-
-        except:
-            traceback.print_exc()
-
+    p = pcap.pcapObject()
+    p.open_live('eth1', 1600, 0, 100)
+    p.setfilter('inbound', 0, 0)
+    while 1:
+        p.dispatch(1, process_packet)   
 
 
         
