4b3ade3c20525d099f6bca5be22e3bab0b0eb568
Michael Rigoni <michael.rigoni@origin-nexus.com>
2014-10-22 18:59:20 +0200

    make redis pipelines available in synapse

--- origin/neuron.py
+++ origin/neuron.py
@@ -20,15 +20,46 @@ class Synapse(redis.StrictRedis):
     '''
     pool = redis.ConnectionPool(decode_responses=True)
     
+    def get_unique_id(self, path='synapse:id'):
+        '''
+            Return a unique integer ID (incremented at each call)
+        '''
+        return self.incrby(path)
+
+    def pipeline(self, transaction=True, shard_hint=None):
+        return SynapsePipeline(
+            self.connection_pool,
+            self.response_callbacks,
+            transaction,
+            shard_hint)
+
     def __init__(self):
         super(Synapse, self).__init__(connection_pool=self.pool)
+        self.from_json_callbacks = {
+                'GET':           self.parse_single_object,
+                'HGETALL':       self.parse_hgetall,
+                'HGET':          self.parse_single_object,
+                'SMEMBERS':      self.parse_smembers, 
+                'ZRANGE':        self.parse_zrange,
+                'ZRANGEBYSCORE': self.parse_zrange,
+                'LRANGE':        self.parse_lrange,
+                'LPOP':          self.parse_single_object,
+                'BLPOP':         self.parse_bpop,
+                'BRPOP':         self.parse_bpop,
+        }
+        
+    def parse_response(self, connection, command_name, **options):
+        response = super(Synapse, self).parse_response(connection, command_name, **options)
+        if command_name in self.from_json_callbacks:
+            return self.from_json_callbacks[command_name](response, **options)
+        return response
 
-    def get(self, key):
-        ''' returns JSON decoded object in key '''
-        data = super(Synapse, self).get(key)
-        if data == None:
+
+    def parse_single_object(self, response, **options):
+        if response == None:
             return None
-        return json.loads(data)
+        return json.loads(response)
+
     def set(self, key, value, *args, **kwargs):
         ''' JSON encodes object and stores it '''
         return super(Synapse, self).set( key, json.dumps(value, sort_keys=True), *args, **kwargs )
@@ -75,14 +106,8 @@ class Synapse(redis.StrictRedis):
             pipe.execute()
 
     # Hashes
-    def hgetall(self, key):
-        return { k: json.loads(v) for k, v in super(Synapse, self).hgetall(key).items() }
-
-    def hget(self, key, field):
-        data = super(Synapse, self).hget(key, field)
-        if data == None:
-            return None
-        return json.loads(data)
+    def parse_hgetall(self, response, **options):
+        return { k: json.loads(v) for k, v in response.items() }
 
     def hset(self, key, field, value):
         return super(Synapse, self).hset(key, field, json.dumps(value, sort_keys=True))
@@ -97,8 +122,8 @@ class Synapse(redis.StrictRedis):
     def srem(self, key, *args):
         return super(Synapse, self).srem(key, *(json.dumps(v, sort_keys=True) for v in args))
         
-    def smembers(self, key):
-        return set(json.loads(v) for v in super(Synapse, self).smembers(key))
+    def parse_smembers(self, response, **options):
+        return set(json.loads(v) for v in response)
 
     # oredered sets
     def zadd(self, key, *args):
@@ -120,36 +145,26 @@ class Synapse(redis.StrictRedis):
     def zscore(self, key, value):
         return super(Synapse, self).zscore(key, json.dumps(value, sort_keys=True))
 
-    def zrange(self, key, begin, end, withscores=False):
-        if withscores:
-            return [(json.loads(v[0]), v[1]) for v in super(Synapse, self).zrange(key, begin, end, withscores=True)]
+    def parse_zrange(self, response, **options):
+        if options.get('withscores', False):
+            return [(json.loads(v[0]), v[1]) for v in response]
         else:
-            return [json.loads(v) for v in super(Synapse, self).zrange(key, begin, end, withscores=False)]
-
-    def zrangebyscore(self, key, begin, end, withscores=False):
-        if withscores:
-            return [(json.loads(v[0]), v[1]) for v in super(Synapse, self).zrangebyscore(key, begin, end, withscores=True)]
-        else:
-            return [json.loads(v) for v in super(Synapse, self).zrangebyscore(key, begin, end, withscores=False)]
+            return [json.loads(v) for v in response]
 
     # Lists
     def lmembers(self, key):
-        return [json.loads(v) for v in super(Synapse, self).lrange(key, 0, -1)]
+        return self.lrange(key, 0, -1)
+    
+    def parse_lrange(self, response, **options):
+        return [json.loads(v) for v in response]
 
     def lpush(self, key, *args):
         return super(Synapse, self).lpush(key, *(json.dumps(v, sort_keys=True) for v in args))
     
-    def lpop(self, key):
-        data = super(Synapse, self).lpop(key)
-        if data == None:
+    def parse_bpop(self, response, **options):
+        if response == None:
             return None
-        return json.loads(data)
-
-    def blpop(self, key, timeout = 0):
-        data = super(Synapse, self).blpop(key, timeout)
-        if data == None:
-            return None
-        return (data[0], json.loads(data[1]))
+        return (response[0], json.loads(response[1]))
 
     def rpush(self, key, *args):
         return super(Synapse, self).rpush(key, *(json.dumps(v, sort_keys=True) for v in args))
@@ -160,22 +175,6 @@ class Synapse(redis.StrictRedis):
             return None
         return json.loads(data)
 
-    def brpop(self, key, timeout = 0):
-        data = super(Synapse, self).brpop(key, timeout)
-        if data == None:
-            return None
-        return (data[0], json.loads(data[1]))
-
-    def pipeline(self):
-        return super(Synapse, self).pipeline()
-
-    def transaction(self, func, *watches, **kwargs):
-        # create a specific pipeline each time...
-        return super(Synapse, self).transaction(func, *watches, **kwargs)
-    
-    def exists(self, key):
-        return super(Synapse, self).exists(key)
-
     def publish(self, channel, msg):
         return super(Synapse, self).publish(channel, json.dumps(msg, sort_keys=True))
 
@@ -185,11 +184,12 @@ class Synapse(redis.StrictRedis):
     def pubsub(self):
         return super(Synapse, self).pubsub()
 
-    def keys(self, pattern):
-        return super(Synapse, self).keys(pattern)
+class SynapsePipeline(redis.client.BasePipeline, Synapse):
+    "Pipeline for the Synapse class"
+    pass
 
 
-class Dendrite:
+class Dendrite(object):
     """ Dendrite is the interface to the Central Controller
         
         Subclass this or provide answer_cb and call_cb to constructor.
@@ -243,12 +243,12 @@ class Dendrite:
     
     def run_for_ever(self):
         while True:
-            tuple = self.synapse.brpop(self.channel_cb.keys(), self.timeout)
+            channel_data_tuple = self.synapse.brpop(self.channel_cb.keys(), self.timeout)
             
-            if tuple is None: # Timeout
+            if channel_data_tuple is None: # Timeout
                 self.timeout_cb()
             else:
-                channel, data = tuple
+                channel, data = channel_data_tuple
                 # Special cases for common channels call and answer 
                 if channel == AXON_CALLS.format(name=self.name):
                     path = data['path']
