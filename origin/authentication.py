ed63f0c1400b31d83629a91cce25a213465d7ec4
Michael Rigoni <michael.rigoni@origin-nexus.com>
2015-04-23 17:30:05 +0200

    allow smart group in group

--- origin/authentication.py
+++ origin/authentication.py
@@ -33,12 +33,67 @@ class AuthenticationProvider(Dendrite):
         super().__init__('authentication')
 
         self.agent_id = None
-        self.providers = {} # indexed by id
-        self.groups = {}  # indexed by id
+        self.authentications = {} # indexed by id
 
         self.retrieve('agent/self')
         self.subscribe('authentication')
+        
+        self.policy_template = Template(filename="/origin/authentication/freeradius/policy")
+        self.ldap_template = Template(filename="/origin/authentication/freeradius/ldap-module")
+        self.cc_auth_template = Template('''
+            update session-state {
+                &Origin-Auth-Provider := ${id}
+            }
+            cc-auth {
+                fail = 1
+            }
+        ''')
+        self.ldap_auth_template = Template('''
+            update session-state {
+                &Origin-Auth-Provider := ${id}
+            }
+            ldap-auth-${id} {{
+                fail = 1
+            }
+            if(fail) {
+                update reply {
+                    Reply-Message += &Module-Failure-Message
+                }
+                update request {
+                    Module-Failure-Message !* ''
+                }
+            }
+        ''')
+        self.rest_conf = Template(filename="/origin/authentication/freeradius/rest-module")
 
+
+
+    def get_group_inner_case(self, auth, ignore_authentications=None):
+        if ignore_authentications is None:
+            ignore_authentications = set()
+        
+        if auth['id'] in ignore_authentications:
+            # authentications has already been tried. no need to try it again...
+            return ''
+
+        ignore_authentications.add(auth['id'])
+
+        inner_case = ''
+        
+        if auth['type'] == 'group':
+            for member_id in auth('members'):
+                inner_case += self.get_group_inner_case( self.authentications[member_id], ignore_authentications )
+        else:
+            inner_case = 'if( notfound || fail ) {\n'   
+
+            if auth['type'] == 'LDAP' and self.agent_id in auth['agents']:
+                inner_case += self.ldap_auth_template.render(**auth)
+            else:
+                inner_case += self.cc_auth_template.render(**auth)
+    
+            inner_case += "\n}\n"
+        
+        return inner_case
 
     def answer_cb(self, path, conf):
         conf_changed = False
@@ -48,107 +103,47 @@ class AuthenticationProvider(Dendrite):
                 self.agent_id = conf['id']
                 conf_changed = True
         elif path == 'authentication':
-            new_groups = {}
-            new_providers = {}
+            new_authentications = {}
             for auth in conf:
-                if auth['type'] == 'group':
-                    new_groups[auth['id']] = auth
-                else:
-                    new_providers[auth['id']] = auth
+                new_authentications[auth['id']] = auth
             
-            if new_groups != self.groups:
-                self.groups = new_groups
-                conf_changed = True 
-            if new_providers != self.providers:
-                self.providers = new_providers
+            if new_authentications != self.authentications:
+                self.authetications = new_authentications
                 conf_changed = True 
 
         if self.agent_id and conf_changed: # we may receive agent id after conf
             # Grab templates
-            policy_template = Template(filename="/origin/authentication/freeradius/policy")
-            ldap_template = Template(filename="/origin/authentication/freeradius/ldap-module")
-            cc_auth_template = Template('''
-                update session-state {
-                    &Origin-Auth-Provider := ${id}
-                }
-                cc-auth {
-                    fail = 1
-                }
-            ''')
-            rest_conf = Template(filename="/origin/authentication/freeradius/rest-module")
-
-            module_conf = rest_conf.render(agent_id=self.agent_id)
+            module_conf = self.rest_conf.render(agent_id=self.agent_id)
     
             inner_switch_server_conf = ""
             # Generate the files if we have all the information...
             new_provided_services = set()
-            for provider in self.providers.values():
-                if provider['type'] == 'LDAP':
-                    if self.agent_id in provider['agents']:
-                        module_conf += "\n" + ldap_template.render(**provider)
-                        inner_switch_server_conf += '''
+            
+            for auth in self.authentications.values():
+                if auth['type'] == 'LDAP' and self.agent_id in auth['agents']:
+                    module_conf += "\n" + self.ldap_template.render(**auth)
+                    inner_switch_server_conf += self.ldap_auth_template.render(**auth)
+                    # also notify that we provide this auth
+                    new_provided_services.add( 'authentication/provider/{id}/authenticate'.format(id=auth['id']) )
+                elif auth['type'] == 'group':    
+                    # Take care of groups, that can be nested:
+                    inner_switch_server_conf +=  '''
                             case {id} {{
-                                update session-state {{
-                                    &Origin-Auth-Provider := {id}
-                                }}
-                                ldap-auth-{id} {{
-                                    fail = 1
-                                }}
-                                if(fail) {{
-                                    update reply {{
-                                        Reply-Message += &Module-Failure-Message
-                                    }}
-                                    update request {{
-                                        Module-Failure-Message !* ''
-                                    }}
-                                }}
+                                notfound
+                                {inner_case}
                             }}
-                        '''.format(**provider)
-                        # also notify that we provide this auth
-                        new_provided_services.add( 'authentication/provider/{id}/authenticate'.format(id=provider['id']) )
-            for group in self.groups.values():
-                first = True
-                inner_case = ""
-                for provider_id in group['providers']:
-                    provider_conf = self.providers[ provider_id ]
-                    if not first:
-                        inner_case += "if( notfound || fail ) {\n"
-                    if provider_conf['type'] == 'LDAP' and self.agent_id in provider_conf['agents']:
-                        inner_case += '''
-                        update session-state {{
-                            &Origin-Auth-Provider := {id}
-                        }}
-                        ldap-auth-{id} {{
-                            fail = 1
-                        }}
-                        if(fail) {{
-                            update reply {{
-                                Reply-Message += &Module-Failure-Message
-                            }}
-                            update request {{
-                                Module-Failure-Message !* ''
-                            }}
-                        }}
-                        '''.format(**provider_conf)
-                    else:
-                        inner_case += cc_auth_template.render(**provider_conf)
-                    if not first:
-                        inner_case += "\n}\n"
-                    else :
-                        first = False
-                inner_switch_server_conf +=  '''
-                        case {id} {{
-                            {inner_case}
-                        }}
-                    '''.format(id=group['id'], inner_case=inner_case)
-    
+                    '''.format(
+                           id = auth['id'],
+                           inner_case = self.get_group_inner_case(auth) )
+
+
             with open ("/etc/freeradius/mods-enabled/authentications", "w") as module_file:
                 module_file.write( module_conf )
             with open ("/etc/freeradius/policy.d/authentications", "w") as policy_file:
-                policy_file.write( policy_template.render(inner_switch=inner_switch_server_conf) )
+                policy_file.write( self.policy_template.render(inner_switch=inner_switch_server_conf) )
             
             # CAs
-            for provider in self.providers.values():
+            for provider in self.authentications.values():
                 if provider.get('server_ca', None):
                     with open ("/etc/freeradius/certs/server_CA/auth-{id}.pem".format(id=provider['id']), "w") as server_ca_file:
                         server_ca_file.write(provider['server_ca'])
