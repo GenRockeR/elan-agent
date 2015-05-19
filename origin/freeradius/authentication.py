f5f1836770705978cb570cddbcb8cdbfd1245760
Michael Rigoni <michael.rigoni@origin-nexus.com>
2015-05-19 16:22:39 +0200

    sometimes we do not have the details of the authentication failure...

--- origin/freeradius/authentication.py
+++ origin/freeradius/authentication.py
@@ -10,10 +10,14 @@ def AuthenticationProviderFailed(req, level='danger'):
         request = request_as_hash_of_values(req)
         
         # Todo: set source depending on requester: radius-dot1x, captive-portal-web or captive-portal-guest-access
-        Event('runtime-failure-authentication', source='radius', level=level)\
-            .add_data('details', request.get('Module-Failure'))\
-            .add_data('authentication_provider', request.get('Origin-Auth-Failed'), data_type='authentication')\
-            .notify()
+        event = Event('runtime-failure-authentication', source='radius', level=level)\
+            .add_data('authentication_provider', request.get('Origin-Auth-Failed'), data_type='authentication')
+        
+        if request.get('Module-Failure-Message'):
+            event.add_data('details', request.get('Module-Failure-Message'))
+
+        event.notify()
+        
     except:
         ExceptionEvent(source='radius').notify()
         raise
