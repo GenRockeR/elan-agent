7b612a045ccf5fa0f5f66b40ca200da7aa4c80de
Michael Rigoni <michael.rigoni@origin-nexus.com>
2016-06-08 19:07:06 +0200

    first implementation of auto captive portal when no allowed on VLAN or accessing VLAN not allowed

---
+++ exec/redirector.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import os
+from origin.event import ExceptionEvent
+
+REDIRECTOR_NFLOG_QUEUE = int(os.environ.get('REDIRECTOR_NFLOG_QUEUE', 20))
+
+
+if __name__ == '__main__':
+    import origin.libnflog_cffi
+    from origin.neuron import Dendrite
+    from impacket.ImpactDecoder import EthDecoder
+    from subprocess import call
+    
+    dendrite = Dendrite('connection-redirector')
+
+    nflog = origin.libnflog_cffi.NFLOG().generator(REDIRECTOR_NFLOG_QUEUE, extra_attrs=['msg_packet_hwhdr', 'nfmark'], nlbufsiz=2**24, handle_overflows = False)
+    fd = next(nflog)
+    
+    decoder = EthDecoder()
+    
+     
+    for pkt, hwhdr, nfmark in nflog:
+        try:
+            eth_obj = decoder.decode(pkt)
+            ip_obj = eth_obj.child()
+            tcp_obj = ip_obj.child()
+            
+            if ip_obj.__class__.__name__ == 'IP':
+                family = 'ip'
+                src_ip = ip_obj.get_ip_src()
+                dst_ip   = ip_obj.get_ip_dst()
+            elif ip_obj.__class__.__name__ == 'IP6':
+                family = 'ip6'
+                src_ip = ip_obj.get_source_address().as_string()
+                dst_ip = ip_obj.get_destination_address().as_string()
+            
+            call([  'nft', 'add','element', family, 'origin', 'redirect_marks', 
+                    '{{ {src_ip} . {src_port} . {dst_ip} . {dst_port} : {mark} }}'.format(
+                                        src_ip   = ip_obj.get_ip_src(),
+                                        src_port = tcp_obj.get_th_sport(),
+                                        dst_ip   = ip_obj.get_ip_dst(),
+                                        dst_port = tcp_obj.get_th_dport(),
+                                        mark = nfmark
+                    )
+            ])
+            # TODO : reinject the packet instead of waiting for client retry
+            # TODO : Cleanup maps afet 120 seconds or when in input (nat has be made)
+
+        except Exception as e:
+            ExceptionEvent(source='network')\
+                 .add_data('packet', pkt)\
+                 .add_data('hw_header', hwhdr)\
+                 .notify()
+
+
+
+
+        
+            
