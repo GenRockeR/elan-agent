58e2c20b93dffeac8741993a4ff72599e2d8b51c
Michael Rigoni <michael.rigoni@origin-nexus.com>
2016-06-09 17:04:40 +0200

    do not fork nft on evry redirection

--- exec/redirector.py
+++ exec/redirector.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 from origin.event import ExceptionEvent
+from __future__ import print_function
+import subprocess
 
 REDIRECTOR_NFLOG_QUEUE = int(os.environ.get('REDIRECTOR_NFLOG_QUEUE', 20))
 
@@ -18,7 +20,10 @@ if __name__ == '__main__':
     
     decoder = EthDecoder()
     
-     
+    nft_process = subprocess.Popen(['nft', '-i'], stdin=subprocess.PIPE)
+    def nft(cmd):
+        print(cmd, file=nft_process.stdin)
+    
     for pkt, hwhdr, nfmark in nflog:
         try:
             eth_obj = decoder.decode(pkt)
@@ -34,17 +39,21 @@ if __name__ == '__main__':
                 src_ip = ip_obj.get_source_address().as_string()
                 dst_ip = ip_obj.get_destination_address().as_string()
             
-            call([  'nft', 'add','element', family, 'origin', 'redirect_marks', 
-                    '{{ {src_ip} . {src_port} . {dst_ip} . {dst_port} : {mark} }}'.format(
+            nft(    'add element {family} origin redirect_marks {{ {src_ip} . {src_port} . {dst_ip} . {dst_port} : {mark} }}'.format(
+                                        family = family,
                                         src_ip   = ip_obj.get_ip_src(),
                                         src_port = tcp_obj.get_th_sport(),
                                         dst_ip   = ip_obj.get_ip_dst(),
                                         dst_port = tcp_obj.get_th_dport(),
                                         mark = nfmark
                     )
-            ])
+            )
             # TODO : reinject the packet instead of waiting for client retry
             # TODO : Cleanup maps afet 120 seconds or when in input (nat has be made)
+        
+        except IOError:
+            nft_process.terminate()
+            nft_process = subprocess.Popen(['nft', '-i'], stdin=subprocess.PIPE, universal_newlines=True)
 
         except Exception as e:
             ExceptionEvent(source='network')\
