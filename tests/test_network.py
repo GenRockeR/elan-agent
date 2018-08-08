76c4b91d1d3df86f92bb1ef6ebe43ce19aa5af80
Michael Rigoni <michael.rigoni@origin-nexus.com>
2018-08-08 19:28:24 +0200

    netplan: do not declare vlans when no vlans are present

--- tests/test_network.py
+++ tests/test_network.py
@@ -13,3 +13,13 @@ class SessionTest(unittest.TestCase):
 
         handle = mock_file()
         handle.write.assert_called_once()
+        self.assertNotIn('vlans', handle.write.call_args[0][0])
+
+    @mock.patch('elan.network.open', new_callable=mock.mock_open)
+    @mock.patch('elan.network.NetworkConfigurator.vlans_conf_template', 'netplan-vlans.yaml')
+    def test_generate_vlans_conf(self, mock_file):
+        network.NetworkConfigurator.generate_vlans_conf_files([{'interface': 'dummy1'}, {'interface': 'dummy2', 'vlan_id': 5}])
+
+        handle = mock_file()
+        handle.write.assert_called_once()
+        self.assertEqual(handle.write.call_args[0][0].count('vlans:'), 1)
